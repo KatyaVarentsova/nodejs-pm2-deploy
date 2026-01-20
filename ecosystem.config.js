5a4e263326ac3f28c1aeb2b05d7a7a57ad4ab047
Ekaterina Varentsova <kotya.v.gr@gmail.com>
2026-01-20 17:21:30 +0300

    fix: 4. Отредактирован сценарий деплой pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,47 +1,34 @@
-require('dotenv').config({ path: '.env.deploy' });
-
-const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  DEPLOY_REPO,
-  DEPLOY_REF = 'origin/master',
-  PORT = 3000,
-  DB_ADDRESS = 'mongodb://localhost:27017/mestodb',
-} = process.env;
-
 module.exports = {
   apps: [
     {
-      name: "backend",
-      script: "./dist/app.js",
-      cwd: "./backend",
+      name: 'backend',
+      script: './dist/app.js',
+      cwd: './backend',
       env_production: {
-        NODE_ENV: "production",
-        PORT,
-        DB_ADDRESS,
+        NODE_ENV: 'production',
+        PORT: process.env.PORT || 3000,
+        DB_ADDRESS: process.env.DB_ADDRESS || 'mongodb://localhost:27017/mestodb',
       },
-    },
+    }
   ],
-
   deploy: {
     production: {
-      user: DEPLOY_USER,
-      host: DEPLOY_HOST,
-      ref: DEPLOY_REF,
-      repo: DEPLOY_REPO,
-      path: DEPLOY_PATH,
-
-      "post-deploy": `
-      cd current/backend &&
-      npm install &&
-      npm run build &&
-      cd ../frontend &&
-      export NODE_OPTIONS=--openssl-legacy-provider &&
-      npm install &&
-      npm run build &&
-      pm2 reload backend --env production
-      `.replace(/\n/g, " "),
-    },
-  },
+      user: process.env.DEPLOY_USER || 'user',
+      host: process.env.DEPLOY_HOST || '158.160.215.176',
+      ref: process.env.DEPLOY_REF || 'origin/master',
+      repo: process.env.DEPLOY_REPO || 'https://github.com/KatyaVarentsova/nodejs-pm2-deploy.git',
+      path: process.env.DEPLOY_PATH || '/home/user/nodejs-pm2-deploy',
+      ssh_options: `StrictHostKeyChecking=no -i ${process.env.DEPLOY_SSH_KEY || '~/.ssh/authorized_keys'}`,
+      'post-deploy': `
+        cd current/backend &&
+        npm install &&
+        npm run build &&
+        cd ../frontend &&
+        export NODE_OPTIONS=--openssl-legacy-provider &&
+        npm install &&
+        npm run build &&
+        pm2 reload backend --env production
+      `.replace(/\n/g, ' '),
+    }
+  }
 };
