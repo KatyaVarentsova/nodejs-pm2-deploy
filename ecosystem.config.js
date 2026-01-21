447bfe96981c72fa04658439d2f481ce69676baf
Ekaterina Varentsova <kotya.v.gr@gmail.com>
2026-01-21 13:13:17 +0300

    fix: 4. Отредактирован сценарий деплой pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
     production: {
       user: process.env.DEPLOY_USER || 'user',
       host: process.env.DEPLOY_HOST || '158.160.215.176',
-      ref: process.env.DEPLOY_REF || 'origin/test',
+      ref: process.env.DEPLOY_REF || 'origin/master',
       repo: process.env.DEPLOY_REPO || 'https://github.com/KatyaVarentsova/nodejs-pm2-deploy.git',
       path: process.env.DEPLOY_PATH || '/home/user/nodejs-pm2-deploy',
       ssh_options: `StrictHostKeyChecking=no -i ${process.env.DEPLOY_SSH_KEY || '~/.ssh/id_ed25519'}`,
