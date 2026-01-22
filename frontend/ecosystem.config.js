3cdf251940f9fb7aa4c22ce0791ee46860d1111e
Ekaterina Varentsova <kotya.v.gr@gmail.com>
2026-01-22 16:50:14 +0300

    Fix: разделён ecosystem.config.js после ревью

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -12,7 +12,8 @@ module.exports = {
       ref: process.env.DEPLOY_REF || 'origin/master',
       repo: process.env.DEPLOY_REPO || 'https://github.com/KatyaVarentsova/nodejs-pm2-deploy.git',
       path: process.env.DEPLOY_PATH || '/home/user/nodejs-pm2-deploy',
-      'post-deploy': 'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && cd frontend && npm i && export NODE_OPTIONS=--openssl-legacy-provider && npm run build && sudo mkdir -p /var/www/html && sudo cp -r build/* /var/www/html/ && sudo chown -R www-data:www-data /var/www/html && sudo chmod -R 755 /var/www/html',
+      'post-deploy': 'cd frontend && /home/user/.nvm/versions/node/v22.22.0/bin/npm i && export NODE_OPTIONS=--openssl-legacy-provider && /home/user/.nvm/versions/node/v18.16.0/bin/npm run build && sudo mkdir -p /var/www/html && sudo cp -r build/* /var/www/html/ && sudo chown -R www-data:www-data /var/www/html && sudo chmod -R 755 /var/www/html',
+
     },
   },
 };
