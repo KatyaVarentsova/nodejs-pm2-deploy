60cbafc6d2d2179ac0bd9030240c4a704fdbe6d3
Ekaterina Varentsova <kotya.v.gr@gmail.com>
2026-01-22 16:09:54 +0300

    Fix: разделён ecosystem.config.js после ревью

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: 'frontend',
+  }],
+  deploy: {
+    production: {
+      user: process.env.DEPLOY_USER || 'user',
+      host: process.env.DEPLOY_HOST || '158.160.215.176',
+      ref: process.env.DEPLOY_REF || 'origin/master',
+      repo: process.env.DEPLOY_REPO || 'https://github.com/KatyaVarentsova/nodejs-pm2-deploy.git',
+      path: process.env.DEPLOY_PATH || '/home/user/nodejs-pm2-deploy',
+      'post-deploy': `
+      cd frontend && 
+      npm install && 
+      export NODE_OPTIONS=--openssl-legacy-provider &&
+      npm run build
+      `.replace(/\n/g, ' '),
+      },
+    },
+};
