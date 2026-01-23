c08855a6d820a0b84ae11415559fb3e5c08848f9
Ekaterina Varentsova <kotya.v.gr@gmail.com>
2026-01-23 23:34:28 +0300

    Fix: убраны секьюрные данные после 2ого ревью

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -5,20 +5,20 @@ module.exports = {
       script: 'npm run start',
       env_production: {
         NODE_ENV: 'production',
-        PORT: process.env.PORT || 3000,
-        DB_ADDRESS: process.env.DB_ADDRESS || 'mongodb://localhost:27017/mestodb',
+        PORT: process.env.PORT,
+        DB_ADDRESS: process.env.DB_ADDRESS,
       },
     },
   ],
 
   deploy: {
     production: {
-      user: process.env.DEPLOY_USER || 'user',
-      host: process.env.DEPLOY_HOST || '158.160.215.176',
-      ref: process.env.DEPLOY_REF || 'origin/master',
-      repo: process.env.DEPLOY_REPO || 'https://github.com/KatyaVarentsova/nodejs-pm2-deploy.git',
-      path: process.env.DEPLOY_PATH || '/home/user/nodejs-pm2-deploy',
-      ssh_options: `StrictHostKeyChecking=no -i ${process.env.DEPLOY_SSH_KEY || '~/.ssh/id_ed25519'}`,
+      user: process.env.DEPLOY_USER,
+      host: process.env.DEPLOY_HOST,
+      ref: process.env.DEPLOY_REF,
+      repo: process.env.DEPLOY_REPO,
+      path: process.env.DEPLOY_PATH,
+      ssh_options: `StrictHostKeyChecking=no -i ${process.env.DEPLOY_SSH_KEY}`,
       'post-deploy': `
       export NVM_DIR="$HOME/.nvm" &&
       [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" &&
